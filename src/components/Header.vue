14849352aaa119817765196b33cb41532534a4af
wahab <aderintoadetunji@gmail.com>
2021-06-14 17:27:38 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,74 @@
+<template>
+  <section class="header">
+    <section class="header-left">
+      <section class="header-left-logo">
+        <img src="../assets/logo.png" alt="logo" class="header-left-logo-img">
+        <h3 class="header-left-logo-text">COM</h3>
+      </section>
+      <section class="header-left-search">
+        <Search></Search>
+      </section>
+    </section>
+    <section class="header-right">Right</section>
+  </section>
+</template>
+
+
+<script>
+  import Search from './Search.vue'
+  export default {
+    components: {
+      Search
+    },
+    data () {
+      return {
+        linkedinTooltip: 'Follow us on Linkedin',
+        facebookTooltip: 'Follow us on Facebook',
+        twitterTooltip: 'Follow us on Twitter',
+        instagramTooltip: 'Follow us on Instagram',
+        isCheckoutActive: false,
+        isMenuOpen: false
+      }
+    },
+  }
+</script>
+
+    
+
+    
+  
+
+  
+  
+  
+<style scoped>
+  .header {
+    width:100%;
+    height: 3rem;
+    padding-right: 10px;
+    
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .header-left {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 30%
+  }
+  .header-left-logo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 30%
+  }
+  .header-left-logo-img{
+    width:40%;
+    height: 70%;
+  }
+  .header-left-search {
+    width:60%
+  }
+  
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,46 @@
+<template>
+	<p class="control has-icons-left">
+		<input
+			class="input is-rounded"
+			type="text"
+			v-model="value"
+			placeholder="Search"
+			@keyup="search(value)"
+		>
+		<span class="icon is-small is-left">
+			<i class="fa fa-search"></i>
+		</span>
+	</p>
+</template>
+
+<style scoped>
+  .control {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height:100%;
+      border: 2.5px solid lightgrey;
+      border-radius: 25px;
+      padding: 8px
+      
+    }
+    input{
+        flex: 1;
+        height: 1.5rem;
+        border: none;
+    }
+    input:focus  {
+        border:none;
+        outline: none;
+    }
+    .icon {
+        border: none;
+        color:lightgrey;
+        font-size: 18px;
+    }
+    ::placeholder {
+        color: lightgrey;
+        font-size: 18px;
+    }
+</style>
