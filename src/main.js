f2453ab3fe1b2649d61932866eb1fca7c479cf99
tunji93 <75486000+tunji93@users.noreply.github.com>
2021-06-23 16:07:40 +0100

    changed proxy to alloworigin

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import card from './components/card.vue'
 Vue.use(Router)
 Vue.use(Vuex)
 Vue.config.productionTip = false
-const proxy = `https://cors-anywhere.herokuapp.com/`
+const proxy = `http://alloworigin.com/get?url=`
 const router = new Router({
   mode: 'history',
   routes: [
